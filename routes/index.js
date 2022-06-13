2daa04d8291dba6ae99eeaf06057b9c00752bb2a
jsoyun <syusmm233@gmail.com>
2022-06-13 13:19:39 +0900

    npm init

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  console.log("야야ㅑㅇ");
   res.render("index");
-  console.log("라우터연ㅇ결");
 });
 
 module.exports = router;
